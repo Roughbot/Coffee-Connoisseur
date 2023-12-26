83e159313872d0f413c1814308e39794dd92a637
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2023-12-26 17:47:06 +0530

    update

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,10 @@
 import { useRouter } from "next/Router";
 import Link from "next/link";
+import Head from "next/head";
+import styles from "../../styles/coffee-store.module.css";
 import coffeeStoresData from "../../data/coffee-stores.json";
+import Image from "next/image";
+import cls from "classnames";
 
 export function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -15,21 +19,54 @@ export function getStaticProps(staticProps) {
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
-    fallback: false,
+    paths,
+    fallback: true,
   };
 }
 
 const CoffeeStore = (props) => {
   const Router = useRouter();
 
+  if (Router.isFallback) {
+    return <div>Loading....</div>;
+  }
+
+  const { address, name, neighbourhood, imaUrl } = props.coffeeStore;
+
   return (
-    <div>
-      Coffee Store Page
-      <Link href="/">Back To Home</Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">Back To Home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            className={styles.storeImg}
+            alt={name}
+            src={imaUrl}
+            width={600}
+            height={360}
+          ></Image>
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <p>{address}</p>
+          <p>{neighbourhood}</p>
+        </div>
+      </div>
     </div>
   );
 };
